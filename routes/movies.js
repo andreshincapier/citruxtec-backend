a5bee704bb8a792603e5fa2ab4e79a80f0374667
andreshincapier <andreshincapie82132@gmail.com>
2021-03-04 23:01:22 -0500

    Feature: Architecture was added

--- routes/movies.js
+++ routes/movies.js
@@ -1,63 +1,78 @@
 const express = require('express');
-const {
-    moviesMock
-} = require('../utils/mocks/movies');
+const MoviesService = require('../services/movies');
 
 function moviesApi(app) {
-    const router = express.Router();
-    app.use("/api/movies", router);
+  const router = express.Router();
+  app.use('/api/movies', router);
 
-    router.get("/", async function (req, res, next) {
-        try {
-            const movies = await Promise.resolve(moviesMock);
-            res.status(200).json({
-                data: movies,
-                message: 'movies listed'
-            })
-        } catch (error) {
-            next(error)
-        }
-    });
+  const moviesService = new MoviesService();
 
+  router.get('/', async function (req, res, next) {
+    const { tags } = req.query;
+    try {
+      const movies = await moviesService.getMovies({ tags });
+      res.status(200).json({
+        data: movies,
+        message: 'movies listed',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    router.get("/:movieId", async function (req, res, next) {
-        try {
-            const movies = await Promise.resolve(moviesMock[0]);
-            res.status(200).json({
-                data: movies,
-                message: 'Movie retrieve'
-            })
-        } catch (error) {
-            next(error)
-        }
-    });
+  router.get('/:movieId', async function (req, res, next) {
+    const { movieId } = req.params;
+    try {
+      const movies = await moviesService.getMovie({ movieId });
+      res.status(200).json({
+        data: movies,
+        message: 'Movie retrieve',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    router.post("/:movieId", async function (req, res, next) {
-        try {
-            const createMovie = await Promise.resolve(moviesMock[0].id);
-            res.status(201).json({
-                data: createMovie,
-                message: 'Create movie'
-            })
-        } catch (error) {
-            next(error)
-        }
-    });
+  router.post('/:movieId', async function (req, res, next) {
+    const { body: movie } = req;
+    try {
+      const createMovie = await moviesService.createMovie({ movie });
+      res.status(201).json({
+        data: createMovie,
+        message: 'Create movie',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    router.put("/:movieId", async function (req, res, next) {
-        try {
-            const updateMovie = await Promise.resolve(moviesMock[0].id);
-            res.status(201).json({
-                data: updateMovie,
-                message: 'Movie updated'
-            })
-        } catch (error) {
-            next(error)
-        }
-    });
+  router.put('/:movieId', async function (req, res, next) {
+    const { movieId } = req.params;
+    const { body: movie } = req;
 
+    try {
+      const updateMovie = await moviesService.updateMovie({ movieId, movie });
+      res.status(200).json({
+        data: updateMovie,
+        message: 'Movie updated',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
+  router.delete('/:movieId', async function (req, res, next) {
+    const { movieId } = req.params;
+    try {
+      const deletedMovieId = await moviesService.deleteMovie({ movieId });
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'Movie deleted',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 }
-
 
 module.exports = moviesApi;
--- utils/mocks/movies.js
+++ utils/mocks/movies.js
@@ -1,85 +1,86 @@
-const moviesMock = [{
-        "id": 1,
-        "first_name": "Sigvard",
-        "last_name": "Peniello",
-        "email": "speniello0@list-manage.com",
-        "gender": "Genderqueer",
-        "ip_address": "4.237.156.144"
-    },
-    {
-        "id": 2,
-        "first_name": "Giustino",
-        "last_name": "Bygrave",
-        "email": "gbygrave1@blogs.com",
-        "gender": "Non-binary",
-        "ip_address": "247.191.162.75"
-    },
-    {
-        "id": 3,
-        "first_name": "Stewart",
-        "last_name": "Jearum",
-        "email": "sjearum2@forbes.com",
-        "gender": "Genderqueer",
-        "ip_address": "102.28.238.56"
-    },
-    {
-        "id": 4,
-        "first_name": "Marinna",
-        "last_name": "Piaggia",
-        "email": "mpiaggia3@diigo.com",
-        "gender": "Polygender",
-        "ip_address": "11.66.63.0"
-    },
-    {
-        "id": 5,
-        "first_name": "Sophia",
-        "last_name": "Fallowes",
-        "email": "sfallowes4@google.ca",
-        "gender": "Genderqueer",
-        "ip_address": "51.137.135.139"
-    },
-    {
-        "id": 6,
-        "first_name": "Andrea",
-        "last_name": "Liddon",
-        "email": "aliddon5@joomla.org",
-        "gender": "Polygender",
-        "ip_address": "248.79.140.22"
-    },
-    {
-        "id": 7,
-        "first_name": "Aleta",
-        "last_name": "Halsall",
-        "email": "ahalsall6@jalbum.net",
-        "gender": "Non-binary",
-        "ip_address": "218.247.145.73"
-    },
-    {
-        "id": 8,
-        "first_name": "Darren",
-        "last_name": "Gatrell",
-        "email": "dgatrell7@flickr.com",
-        "gender": "Male",
-        "ip_address": "193.79.3.67"
-    },
-    {
-        "id": 9,
-        "first_name": "Conchita",
-        "last_name": "Glancey",
-        "email": "cglancey8@webeden.co.uk",
-        "gender": "Non-binary",
-        "ip_address": "66.155.101.4"
-    },
-    {
-        "id": 10,
-        "first_name": "Leontyne",
-        "last_name": "Dundendale",
-        "email": "ldundendale9@amazonaws.com",
-        "gender": "Genderfluid",
-        "ip_address": "209.56.41.250"
-    }
+const moviesMock = [
+  {
+    id: 1,
+    first_name: 'Sigvard',
+    last_name: 'Peniello',
+    email: 'speniello0@list-manage.com',
+    gender: 'Genderqueer',
+    ip_address: '4.237.156.144',
+  },
+  {
+    id: 2,
+    first_name: 'Giustino',
+    last_name: 'Bygrave',
+    email: 'gbygrave1@blogs.com',
+    gender: 'Non-binary',
+    ip_address: '247.191.162.75',
+  },
+  {
+    id: 3,
+    first_name: 'Stewart',
+    last_name: 'Jearum',
+    email: 'sjearum2@forbes.com',
+    gender: 'Genderqueer',
+    ip_address: '102.28.238.56',
+  },
+  {
+    id: 4,
+    first_name: 'Marinna',
+    last_name: 'Piaggia',
+    email: 'mpiaggia3@diigo.com',
+    gender: 'Polygender',
+    ip_address: '11.66.63.0',
+  },
+  {
+    id: 5,
+    first_name: 'Sophia',
+    last_name: 'Fallowes',
+    email: 'sfallowes4@google.ca',
+    gender: 'Genderqueer',
+    ip_address: '51.137.135.139',
+  },
+  {
+    id: 6,
+    first_name: 'Andrea',
+    last_name: 'Liddon',
+    email: 'aliddon5@joomla.org',
+    gender: 'Polygender',
+    ip_address: '248.79.140.22',
+  },
+  {
+    id: 7,
+    first_name: 'Aleta',
+    last_name: 'Halsall',
+    email: 'ahalsall6@jalbum.net',
+    gender: 'Non-binary',
+    ip_address: '218.247.145.73',
+  },
+  {
+    id: 8,
+    first_name: 'Darren',
+    last_name: 'Gatrell',
+    email: 'dgatrell7@flickr.com',
+    gender: 'Male',
+    ip_address: '193.79.3.67',
+  },
+  {
+    id: 9,
+    first_name: 'Conchita',
+    last_name: 'Glancey',
+    email: 'cglancey8@webeden.co.uk',
+    gender: 'Non-binary',
+    ip_address: '66.155.101.4',
+  },
+  {
+    id: 10,
+    first_name: 'Leontyne',
+    last_name: 'Dundendale',
+    email: 'ldundendale9@amazonaws.com',
+    gender: 'Genderfluid',
+    ip_address: '209.56.41.250',
+  },
 ];
 
 module.exports = {
-    moviesMock
-}
+  moviesMock,
+};
